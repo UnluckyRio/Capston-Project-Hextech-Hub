ffe0a4558acdcc3e116e7eabdc928e6f1efc0dab
Valerio Di Felice <valesmile.99@gmail.com>
2025-11-26 14:39:04 +0100

    update

--- Frontend/src/Main.tsx
+++ Frontend/src/Main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from "./context/AuthContext.tsx";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
-      <BrowserRouter basename="/Hextech-Hub">
+      <BrowserRouter basename="/Capston-Project-Hextech-Hub">
         <App />
       </BrowserRouter>
     </AuthProvider>
